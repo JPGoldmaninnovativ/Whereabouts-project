046fec88e16547970dd5001c34d65396692a514f
annalundberg <jpgoldmaninnovativ@gmail.com>
2018-06-15 14:32:17 +0200

    Modify new HomeScreen.js file

--- HomeScreen.js
+++ HomeScreen.js
@@ -11,7 +11,7 @@ import {
 } from 'react-native';
 
 
-import { Icon, Button, Container, Header, Content, Left, Body, Thumbnail, width } from 'native-base'
+import { Icon, Button, Container, Header, Content, Left, Body, width } from 'native-base'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 
@@ -36,42 +36,46 @@ export default class HomeScreen extends React.Component {
                         </Left>
                     </Header>
 
-                    <View style={styles.top}>
-                        {/* <View style={styles.image}></View> */}
-                        <Text style={styles.text}>H O M E</Text>
+
+
+                    <View style={styles.overlayContainer}>
+                        <View style={styles.top}>
+                            <Text style={styles.header}>H O M E</Text>
+                        </View>
+                        <View style={styles.menuContainer}>
+                        
+                        </View>
                     </View>
+
                     <View style={styles.center}></View>
 
-            
-            
-            
-            
+
                     <View style={styles.photoGrid}>
                         <TouchableOpacity onPress={this.group} >
-                           
-                                <View style={styles.photoWrap}>
-                                    <Image style={styles.photo} source={require('./assets/Umeabg.jpg')} />
+
+                            <View style={styles.photoWrap}>
+                                <Image style={styles.photo} source={require('./assets/Umeabg.jpg')} />
                             </View>
                         </TouchableOpacity>
 
 
                         <TouchableOpacity onPress={this.live}>
-                                <View style={styles.photoWrap}>
-                                    <Image style={styles.photo} source={require('./assets/group.jpg')} />
-                                </View>
+                            <View style={styles.photoWrap}>
+                                <Image style={styles.photo} source={require('./assets/group.jpg')} />
+                            </View>
                         </TouchableOpacity>
 
 
                         <TouchableOpacity onPress={this.event} >
-                                <View style={styles.photoWrap}>
-                                    <Image style={styles.photo} source={require('./assets/event.jpg')} />
-                                </View>
+                            <View style={styles.photoWrap}>
+                                <Image style={styles.photo} source={require('./assets/event.jpg')} />
+                            </View>
                         </TouchableOpacity>
 
                         <TouchableOpacity onPress={this.todo} >
-                                <View style={styles.photoWrap}>
-                                    <Image style={styles.photo} source={require('./assets/todo.jpg')} />
-                                </View>
+                            <View style={styles.photoWrap}>
+                                <Image style={styles.photo} source={require('./assets/todo.jpg')} />
+                            </View>
                         </TouchableOpacity>
 
 
@@ -86,12 +90,20 @@ const styles = StyleSheet.create({
     Container: {
         flex: 1,
     },
-    top: {
+    overlayContainer: {
         height: '35%',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#98d2c1'
+        backgroundColor: 'rgba(127,188,172,1)'
     },
+
+    top: {
+        height: '50%',
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+
+
     image: {
         width: 150,
         height: 150,
@@ -102,29 +114,36 @@ const styles = StyleSheet.create({
     },
     center: {
         height: '10%',
-        backgroundColor: '#7fbcac'
+        backgroundColor: 'rgba(127,188,172,1)'
     },
     photoGrid: {
         flexDirection: 'row',
         flexWrap: 'wrap'
     },
-    photoWrap:{
+    photoWrap: {
         margin: 2,
         height: 180,
         width: (Dimensions.get('window').width / 2) - 4,
- },
-    photo:{
+    },
+    photo: {
         flex: 1,
         width: null,
         alignSelf: 'stretch'
-},
-text: {
-    color: '#fff',
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    margin: 10,
-    fontStyle: 'normal'
-  }
+    },
+    header: {
+        color: '#fff',
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 10,
+        fontStyle: 'normal',
+        borderColor:'#ffff',
+        borderWidth: 2,
+        padding: 20,
+        paddingLeft:  40,
+        paddingRight: 40,
+        backgroundColor: 'rgba(127,188,172,1)'
+
+    }
 });
 
